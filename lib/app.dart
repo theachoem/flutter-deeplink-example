0ac860023830039464bbfc06ca3224130773b3b4
theacheng <theacheng.g6@gmail.com>
2021-08-28 01:20:30 +0700

    initial unilink

--- lib/app.dart
+++ lib/app.dart
@@ -2,12 +2,19 @@ import 'package:deeplink/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  final String? url;
+
+  const App({
+    Key? key,
+    this.url,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: HomScreen(title: 'Flutter Demo Home Page'),
+      home: HomScreen(title: url.toString()),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:deeplink/app.dart';
 import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String? url = await _initialUrl();
+  runApp(
+    StreamBuilder<String?>(
+      stream: linkStream,
+      builder: (context, snapshot) {
+        return App(url: snapshot.data ?? url);
+      },
+    ),
+  );
+}
+
+Future<String?> _initialUrl() async {
+  String? _initialUrl;
+  try {
+    _initialUrl = await getInitialLink();
+  } catch (e) {}
+  return _initialUrl;
 }
